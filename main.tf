1d7c2f9fe461c1f0ffe52f49e664d9dc57821cc6
Jonathan Woodlief <jon13w@gmail.com>
2023-03-30 13:03:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 locals {
     nullcheck = var.aminull == null ? "yes" : "no"
+    blankcheck = var.aminull == "" ? "yes" : "no"
+
 }
 data "template_file" "test" {
-  template = "Hello ${var.nodefault} ${var.name}! null? ${local.nullcheck}"
+  template = "Hello ${var.nodefault} ${var.name}! null? ${local.nullcheck}. blank? ${local.blankcheck}"
 }
